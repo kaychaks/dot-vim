6d3bccef49b1f8d4a916e61bc22659b371cc67bd
Kaushik Chakraborty <git@kaushikc.org>
2023-07-16 09:32:04 +0530

    chore: precision update

---
+++ custom/lua/lang/plugins_setup/lspsaga.lua
@@ -0,0 +1,17 @@
+local saga = require("lspsaga")
+local Lib = require("lib")
+
+saga.setup({
+	lightbulb = {
+		enable = false,
+	}
+})
+
+Lib.nnoremap({
+	["K"] = "<Cmd>Lspsaga hover_doc<CR>",
+	["gd"] = "<Cmd>Lspsaga lsp_finder<CR>",
+	["<C-j>"] = "<Cmd>Lspsaga diagnostic_jump_next<CR>",
+	["gp"] = "<Cmd>Lspsaga peek_definition<CR>",
+	["gr"] = "<Cmd>Lspsaga rename<CR>",
+	["<Leader>ca"] = "<Cmd>Lspsaga code_action<CR>",
+})
